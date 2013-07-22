1ea5b3bc1cf0b95a07970891ba212f894e5db4e6
G. Blake Meike <blake.meike@gmail.com>
2013-07-22 17:46:32 -0400

    Basic status page

---
+++ Yamba/src/com/marakana/android/yamba/StatusActivity.java
@@ -0,0 +1,41 @@
+
+package com.marakana.android.yamba;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.util.Log;
+
+public class StatusActivity extends Activity {
+    private static final String TAG = "MAIN";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        Log.d(TAG, "onCreate");
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_status);
+    }
+
+    @Override
+    protected void onPause() {
+        Log.d(TAG, "onPause");
+        super.onPause();
+    }
+
+    @Override
+    protected void onResume() {
+        Log.d(TAG, "onResume");
+        super.onResume();
+    }
+
+    @Override
+    protected void onStart() {
+        Log.d(TAG, "onStart");
+        super.onStart();
+    }
+
+    @Override
+    protected void onStop() {
+        Log.d(TAG, "onStop");
+        super.onStop();
+    }
+}
